1ef92a8c5dd30f3076aa9c3534791c7611bc4e4f
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2024-03-14 23:55:33 +0100

    fixed register and changed profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/service/user.service';
-import { Observable } from 'rxjs/internal/Observable';
-import { AuthData } from 'src/app/auth/auth-data';
 
 @Component({
   selector: 'app-profile',
@@ -9,35 +7,7 @@ import { AuthData } from 'src/app/auth/auth-data';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  users!: any[];
-  userIsAdmin!: boolean;
-  userLoggedIn!: AuthData | null;
-
   constructor(private userSrv: UserService) {}
 
-  ngOnInit(): void {
-    this.getUsers();
-    this.canActivate().subscribe((isAdmin) => {
-      if (isAdmin) {
-        this.userIsAdmin = true;
-      }
-    });
-  }
-
-  getUsers(): void {
-    this.userSrv.getUsers().subscribe((response: any) => {
-      this.users = response.content;
-    });
-  }
-
-  canActivate(): Observable<boolean> {
-    return this.userSrv.isAdmin();
-  }
-
-  removeUser(id: string) {
-    confirm('Are you sure?');
-    this.userSrv.removeUser(id).subscribe(() => {
-      this.getUsers();
-    });
-  }
+  ngOnInit(): void {}
 }
